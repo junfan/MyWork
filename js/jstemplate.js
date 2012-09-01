1140e605367786b9e77cdfe98ad64d3301b022a0
Fan Jun <powerfj@gmail.com>
2012-09-01 11:35:52 +0800

    old js template

--- js/jstemplate.js
+++ js/jstemplate.js
@@ -10,6 +10,7 @@ var JsTemplate = function() {
     var idxField=/@idx([\+-]\d+)?/;
 
 
+
     function _JsTemplate(tpl) {
         if(tpl){
             this._tplobj = MatchField(tpl);
@@ -23,6 +24,9 @@ var JsTemplate = function() {
         return render(this._tplobj, obj);
     }
 
+	/**
+	 * 获取子对象
+	 */
     _JsTemplate.prototype.getSection=function(name){
         var childNode=this._tplobj.child;
         var childJsTpl;
@@ -39,6 +43,10 @@ var JsTemplate = function() {
         return null;
     }
 
+
+	/**
+	 * 分析单纯的属性
+	 */
     function MatchField(tpl, name, inverse) {
 
         regSec.lastIndex = 0;
@@ -77,6 +85,9 @@ var JsTemplate = function() {
         return obj;
     }
 
+	/**
+	 * 分析属性的实现
+	 */
     function SubMatch(subtpl, parent) {
 
         reg.lastIndex = 0;
@@ -145,6 +156,10 @@ var JsTemplate = function() {
         }
         return escapeHTML(value);
     }
+
+	function nullOrUndef(value){
+		return value===null || typeof subvalue === "undefined";
+	}
 
     function render(tplObj, value,index) {
         value = value || {}
@@ -402,6 +417,7 @@ function JsTemplateTest() {
         }
     }
 
+
     function testAll() {
         testSection();
         testField();
@@ -412,3 +428,127 @@ function JsTemplateTest() {
 
     testAll()
 }
+
+/**
+ * 开发这个模版引擎主要是之前用过一些不好，比较难使用的模版，
+ * 在web前端很多时候我们可能会使用一些innerHTML的方式来构建
+ * 一个代码块，一般这种情况都是通过一个数据与一个模版来进行
+ * 填充的。这里这种模版的思想主要是受到http://mustache.github.com/
+ * 的影响，最大的特点就是无逻辑，无逻辑最大的好处就是逻辑代码
+ * 与生成html的代码进行了分离，这样减少了交叉污染，特别不喜欢
+ * 看到模版代码里面充斥着各种if else语句，for语句，然后导致
+ * 我们需要按照这个模版自己的语法运算一下才能够知道最后生成
+ * 出来的东西长什么样。所以我们鼓励的是尽量减少模版中的逻辑
+ * 处理，这些处理大可以在数据中处理好，然后直接让模版渲染。
+ * 
+ * 为什么需要这样？
+ *
+ * 如果模版中有了太多的逻辑处理的话，我们知道这种代码肯定是很难理解的
+ * 也就是模板做了两个事情，一部分包含了数据处理的逻辑，另外一部分就
+ * 包含了生成文本的工作。而相对来说，模版适合处理的是生成文本，而处理
+ * 数据的逻辑应该放在模版引擎之外，除了小部分通用处理。
+ *
+ */
+
+
+	//这是啥
+	var regSecTag=/\{([#^\/?])([\d\w\+\-\*\/=\>\<=!?@]+)\}/gm
+
+	//寻找分区标记符，主要是为了解决之前的不能匹配嵌套类型的tag的问题
+	//通过使用寻找配对的方式，与括号匹配类似
+	//支持如下的区块：
+	//{#abc}{/abc} 作为一个section，一定match的是一个对象，会递归使用abc对象
+	//{?123=b}{/?} 如果条件为真输出里面的值，不作为区块
+	//{^abc}{/abc} 如果没有abc属性，展示里面的值
+	//
+	function CompileSection(tpl) {
+		regSecTag.lastIndex = 0;
+		var aTopTags = [];
+		var match;
+		var aTags = [];
+		var tag;
+		var parentTag;
+		while (match = regSecTag.exec(tpl)) {
+			tag = {
+				start: match.index,
+				tag: match[2],
+				flag: match[1],
+				end: match[0].length + match.index
+			}
+			//上一个tag
+			lastTag = aTags[aTags.length - 1];
+			//结束标志
+			if ((tag.tag == "?" && tag.flag == "/" && lastTag.flag == "?") || (tag.flag == "/" && tag.tag != "?" && tag.tag == lastTag.tag)) {
+				lastTag.match = tag;
+				aTags.pop();
+				if (aTags.length == 0) {
+					aTopTags.push(lastTag);
+				} else {
+					parentTag = aTags[aTags.length - 1];
+					if (!parentTag.child) {
+						parentTag.child = [];
+					}
+					parentTag.child.push(lastTag);
+				}
+			} else if (tag.flag != "/") {
+				aTags.push(tag);
+			} else {
+				throw new Error("no match end tag");
+			}
+		}
+		//console.log(aTopTags);
+		return aTopTags;
+	}
+
+    var regFieldTag=/\{([><=]?)([\d\w\.@]+)([\+\-\*\/\%][\d]+)?\}/gm
+
+	function ComplileField(tpl){
+		regFieldTag.lastIndex=0;
+	}
+
+	//function CompileTemplate(s){
+
+    function CompileTemplate(tpl, name, inverse) {
+
+        regSec.lastIndex = 0;
+
+        var obj = {
+            type: "section",
+            name: name,
+            inverse: inverse
+        };
+
+        var child = [];
+
+        var lastIndex = 0;
+
+        var submatch, match;
+
+        while (match = regSec.exec(tpl)) {
+            if (match.index > lastIndex) {
+                var subtpl = tpl.substr(lastIndex, match.index - lastIndex)
+                SubMatch(subtpl, child);
+            }
+
+            lastIndex = regSec.lastIndex;
+
+            child.push(MatchField(match[3], match[2], match[1]));
+            //恢复lastIndex
+            regSec.lastIndex = lastIndex;
+        }
+
+        if (tpl.length - lastIndex > 0) {
+            SubMatch(tpl.substr(lastIndex, tpl.length - lastIndex), child)
+        }
+
+        obj.child = child;
+
+        return obj;
+    }
+
+	function testCompile(){
+		var s=" halllo this {?hallo}good{/?} is a tag sec \r\n {#abc} 123 {#abc}good one{/abc}, {?abc=1}haha{/?}\r\n do you want another {^abc} yes {/abc} haha {/abc}"
+		console.log(CompileTemplate(s))
+	}
+
+	testCompile();
